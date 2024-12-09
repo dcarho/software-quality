7cd91810e5366f6052723cc14b53396b4f53ee51
David Carranza <dcarho@outlook.com>
2024-12-09 01:44:33 -0500

    Tests updates

--- Unit tests/eje_test1.py
+++ Unit tests/eje_test1.py
@@ -1,11 +1,19 @@
 from ejercicio import total
 import unittest
 
-class TestCalcularTotal(unittest.TestCase):
+class TestTotal(unittest.TestCase):
+    
+    def setUp(self):
+        global resultado
+        resultado = total([1, 2, 3 , 5])
     
     def test_1(self):
-        resultado = total([1, 2, 3 , 5])
+        
         self.assertEqual(11, resultado)
+    
+    def test_2(self):
+       
+        self.assertNotEqual(12, resultado)
 
 if __name__ == '__main__':
     unittest.main()
--- Unit tests/ejercicio.py
+++ Unit tests/ejercicio.py
@@ -7,31 +7,31 @@ def total(lst):
         accum=accum+x
     return accum
 
-lst=[1,2,3,4]
-res = total(lst)
-print(res)
+#lst=[1,2,3,4]
+#res = total(lst)
+#print(res)
 
 
 # Se agrega 5 al numero que se envia
 def addit(x):
     y = x+5
     return y
-res = addit(3)
-print(res)
+#res = addit(3)
+#print(res)
 
 
 # Multiplica el numero enviado por el numero enviado + 5
 def mult(z):
     q = z * addit(z)
     return q
-res = mult(3)
-print(res)
+#res = mult(3)
+#print(res)
 
 # Divide el numero a enviado entre el numero b enviado
 def divide(a, b):
     return a/b
-res = divide(8, 2)
-print(res)
+#res = divide(8, 2)
+#print(res)
 
 # Calcula el tamaño de una lista o string y devuelve un valor segun el tamaño
 def length(lst):
@@ -39,10 +39,10 @@ def length(lst):
         return "Longer than 5"
     else:
         return "Less than 5"
-res = length([2, 3, 5, 2])
-print(res)
-res = length("Prueba")
-print(res)
+#res = length([2, 3, 5, 2])
+#print(res)
+#res = length("Prueba")
+#print(res)
 
 #Recibe un string y lo devulve al reves -> Ejemplo: casa -> asac
 
@@ -52,8 +52,8 @@ def reverse(mystr):
         reversed = char + reversed
     return reversed
 
-res = reverse("casa")
-print(res)
+#res = reverse("casa")
+#print(res)
 
 #Recibe por parametro un string y una letra y se reemplaza la letra que se requera
 def remove_letter(theLetter, theString):
@@ -62,8 +62,8 @@ def remove_letter(theLetter, theString):
     else:
         return theString
 
-res = remove_letter("a", "Hola esto es una prueba")
-print(res)
+#res = remove_letter("a", "Hola esto es una prueba")
+#print(res)
 
 #Recibe una arrelo de numeros y devuelve el numero mayor
 
@@ -74,8 +74,8 @@ def max(lst):
             max = e
     return max
 
-res = max([1, 8, 3, 0, 12])
-print(res)
+#res = max([1, 8, 3, 0, 12])
+#print(res)
 
 
 #recibe una lista de numeros y devuelve solo los elementos pares
@@ -87,8 +87,8 @@ def even_numbers(mix):
             ev_li.append(i)
     return ev_li
 
-res = even_numbers([1, 8, 3, 6, 12, 5])
-print(res)
+#res = even_numbers([1, 8, 3, 6, 12, 5])
+#print(res)
 
 #recibe una lista de numeros y devuelve solo los elementos impares
 
@@ -98,13 +98,13 @@ def odd_numbers(mix):
         if (i % 2 != 0):
             od_li.append(i)
     return od_li
-res = odd_numbers([1, 8, 3, 6, 12, 5])
-print(res)
+#res = odd_numbers([1, 8, 3, 6, 12, 5])
+#print(res)
 
 #Recibe un numero, y devuelve un booleano, donde indica si es par
 
 def is_even(number):
     return number % 2 == 0
-res = is_even(23)
-print(res)
+#res = is_even(23)
+#print(res)
 
